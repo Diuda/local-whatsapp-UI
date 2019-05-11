fec8cbba9a8a10e792a8f2b81a5337b492cb3e56
Diuda <divyanshu.1996@gmail.com>
2019-05-11 12:37:28 +0530

    user's profile bar initialized

---
+++ src/components/profileBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+class ProfileBar extends React.Component {
+
+
+	render () {
+		return(
+			<div className='profile-wrapper'>
+				<div className='profile-img'>
+
+				</div>
+				<div className='options-wrapper'>
+					<div className='status'>
+
+					</div>
+					<div className='starred'>
+
+					</div>
+					<div className='settings'>
+
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default ProfileBar;
