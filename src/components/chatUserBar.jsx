510eaa9adad35ec7370bb68e0b5e1f3dc8bd7e1b
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:44:47 +0530

    icons and user info in chatbar

--- src/components/chatUserBar.jsx
+++ src/components/chatUserBar.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faPaperclip, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 
 class ProfileBar extends React.Component {
 
 
 	render () {
+
+		const { name, imageURL } = this.props;
 		return(
 			<div className='chat-profile-wrapper'>
 				<div className='chat-sender-profile'>
-					<div className='chat-sender-img'></div>
-					<div className='chat-sender-name'></div>
+					<div className='chat-sender-img'>
+						<img src={`/assets/images/${imageURL}`} alt="friend image"/>
+					</div>
+					<div className='chat-sender-name'>{name}</div>
 				</div>
 				<div className='sender-options-wrapper'>
 					<div className='search-chat'>
-
+						<FontAwesomeIcon icon={faSearch} />
 					</div>
 					<div className='attachment'>
-
+						<FontAwesomeIcon icon={faPaperclip} />
 					</div>
 					<div className='chat-settings'>
-
+						<FontAwesomeIcon icon={faEllipsisV} />
 					</div>
 				</div>
 			</div>
