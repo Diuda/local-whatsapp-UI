f2d8e165598e22847f357f03cbf15df2372fcc56
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:43:31 +0530

    add handler for search bar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -5,6 +5,21 @@ import React from 'react';
 class Searchbar extends React.Component {
 
 
+	constructor(){
+		super();
+		this.state = {
+			userFilter: ''
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			userFilter: e.target.value
+		})
+		this.props.onChange(e.target.value);
+	}
+
+
 	render () {
 		return (
 			<div className='search-wrapper'>
@@ -12,7 +27,7 @@ class Searchbar extends React.Component {
 
 				</div>
 				<div className='input-search'>
-					<input type="text" name="search"/>
+					<input type="text" name="search" onChange={this.handleChange}/>
 				</div>
 			</div>
 		)
