1397ff06e0bb81936d64686f7d4d6ea2156e5c70
Diuda <divyanshu.1996@gmail.com>
2019-05-11 12:37:48 +0530

    recieve and sent chat bubble initialized

---
+++ src/components/recieveChatBubble.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class RecieveChatBubble extends React.Component {
+
+
+	render() {
+		return(
+			<div className='recieve-msg-wrapper'></div>
+		)
+	}
+}
+
+
+export default RecieveChatBubble;
---
+++ src/components/sentChatBubble.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class SentChatBubble extends React.Component {
+
+
+	render() {
+		return(
+			<div className='sent-msg-wrapper'></div>
+		)
+	}
+}
+
+
+export default SentChatBubble;
