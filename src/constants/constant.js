de1682b58358c5631ad383c9330aef142412cfc0
Diuda <divyanshu.1996@gmail.com>
2019-05-12 05:28:53 +0530

    Message sent constant

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,5 @@
 export const SEND_MESSAGE = 'SEND_MESSAGE';
+export const MESSAGE_SENT = 'MESSAGE_SENT';
 export const RECIEVE_MESSAGE = 'RECIEVE_MESSAGE';
 export const LOAD_MESSAGE = 'LOAD_MESSAGE';
 export const LOAD_USERS = 'LOAD_USERS';
