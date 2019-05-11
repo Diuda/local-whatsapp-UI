c397ae1e0973c5963d72b46136ba7f7083377bba
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:43:14 +0530

    props passed for initial render WIP

--- src/components/sentChatBubble.jsx
+++ src/components/sentChatBubble.jsx
@@ -5,8 +5,14 @@ class SentChatBubble extends React.Component {
 
 
 	render() {
+
+		const { text } = this.props;
 		return(
-			<div className='sent-msg-wrapper'></div>
+			<div className='sent-msg-wrapper tri-right left-top'>
+				<div className='sent-msg'>
+					{text}
+				</div>
+			</div>
 		)
 	}
 }
