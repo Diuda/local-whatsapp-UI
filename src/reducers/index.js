ae408d4be62a3bdc258496aa87c8bceb5e288b5c
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:39:28 +0530

    root reducer for future changes

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+// import { routerReducer } from "react-router-redux";
+// import { i18nReducer } from "react-redux-i18n";
+
+
+import chat from './messages';
+
+
+export const reducers = combineReducers({
+    chat
+})
