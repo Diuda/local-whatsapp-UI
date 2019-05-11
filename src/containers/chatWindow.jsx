bbc71252fecd74b06bed9cd6309489fdf8c5090a
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:40:56 +0530

    chat history functionality and add fallbackUI WIP

--- src/containers/chatWindow.jsx
+++ src/containers/chatWindow.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import ChatUserBar from '../components/chatUserBar';
 import ChatInput from '../components/chatInput';
+import ChatHistory from './chatHistory';
+
 
 
 
 class ChatWindow extends React.Component {
 
-
 	render() {
+
+
+		const activeUser = this.props.activeUser;
+		//TODO fallbackUI
 		return(
-			<div className='chat-wrapper'>
-				<ChatUserBar />
-				<ChatInput />
+			<div className='friend-chat-wrapper'>
+				{
+					activeUser ?
+					(
+						<div>
+							<ChatUserBar name={this.props.users[activeUser-1].name} imageURL={this.props.users[activeUser-1].imageURL} />
+							<ChatHistory messages={this.props.users[activeUser-1].messages} />
+							<ChatInput />
+						</div>
+					):null
+				}
 			</div>
 		)
 	}
 }
 
-export default ChatWindow;
+
+const mapStateToProps = state => {
+	return { 
+		activeUser: state.chat.activeUser,
+		users: state.chat.users
+	};
+}
+
+
+
+export default connect(mapStateToProps, null)(ChatWindow);
