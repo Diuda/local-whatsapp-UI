22be3bc1a576e76cb57242de12bbe900e82ec292
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:39:11 +0530

    initial state defined and load chat history action

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,14 +1,20 @@
-import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE  } from '../constants/constant';
+import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE, USERS_FETCHED, LOAD_CHAT_HISTORY } from '../constants/constant';
 
+
+const initialState = {
+	users: [],
+	activeUser: null
+}
 
 
 //TODO hafl the things
-export default function(state, { type, payload }) {
+export default function chat(state=initialState, { type, payload }) {
 	switch (type) {
 		case SEND_MESSAGE: return { users: [...state.users, payload] };
 		case RECIEVE_MESSAGE: return { users: [...state.users, payload] };
 		case LOAD_MESSAGE: return { users: payload };
-			
+		case USERS_FETCHED: return { ...state, users: payload } ;
+		case LOAD_CHAT_HISTORY: return {...state, activeUser: payload };
 		default: return state;
 	}
 }
