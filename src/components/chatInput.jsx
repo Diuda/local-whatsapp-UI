1a71b46f7ada62e312d4280018470d4837f87ddd
Diuda <divyanshu.1996@gmail.com>
2019-05-12 05:29:10 +0530

    handler for sending message

--- src/components/chatInput.jsx
+++ src/components/chatInput.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { sendMessage } from '../actions/index';
+
 
 class ChatInput extends React.Component {
 
+
+	handleSendMessage = (e) => {
+		if(e.which === 13){
+			this.props.sendMessage(this.props.activeUser, e.target.value)
+			e.target.value = '';
+		}
+	}
 
 	render() {
 		return(
 			<div className='chat-wrapper'>
 				<div className='emoji-btn'></div>
 				<div className='chat-input-wrapper'>
-					<input type="text" name="chat-input"/>
+					<input type="text" name="chat-input" onKeyPress={this.handleSendMessage}/>
 				</div>
 				<div className='voice-btn'></div>
 			</div>
@@ -18,4 +28,16 @@ class ChatInput extends React.Component {
 }
 
 
-export default ChatInput;
+const mapStateToProps = state => {
+	return {
+		activeUser: state.chat.activeUser
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		sendMessage: (id, msg) => dispatch(sendMessage(id, msg))
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatInput);
