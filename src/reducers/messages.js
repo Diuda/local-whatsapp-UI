ff482e798e2ac7321b88eaadfa85367cce484a7d
Diuda <divyanshu.1996@gmail.com>
2019-05-12 05:28:39 +0530

    add new message to store

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,4 +1,4 @@
-import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE, USERS_FETCHED, LOAD_CHAT_HISTORY } from '../constants/constant';
+import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE, USERS_FETCHED, LOAD_CHAT_HISTORY, MESSAGE_SENT } from '../constants/constant';
 
 
 const initialState = {
@@ -10,11 +10,15 @@ const initialState = {
 //TODO hafl the things
 export default function chat(state=initialState, { type, payload }) {
 	switch (type) {
-		case SEND_MESSAGE: return { users: [...state.users, payload] };
+		case MESSAGE_SENT: {
+			state.users[state.activeUser].messages.push(payload) 
+			return { ...state };
+		}
 		case RECIEVE_MESSAGE: return { users: [...state.users, payload] };
 		case LOAD_MESSAGE: return { users: payload };
 		case USERS_FETCHED: return { ...state, users: payload } ;
 		case LOAD_CHAT_HISTORY: return {...state, activeUser: payload };
 		default: return state;
 	}
+
 }
