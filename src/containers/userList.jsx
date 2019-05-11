bb9afa7f06ec2ae3880b77ea834b66461bfa5011
Diuda <divyanshu.1996@gmail.com>
2019-05-12 03:40:04 +0530

    user list populated

--- src/containers/userList.jsx
+++ src/containers/userList.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import UserStrip from '../components/userStrip';
 
 class UserList extends React.Component {
 
 	render() {
+
+		const users = this.props.users; 
 		return(
 			<div className='userlist-wrapper'>
-				<UserStrip />
+
+			{ users && users.map(users => (
+				<UserStrip key={users.id} id={users.id} name={users.name} imageURL={users.imageURL} latestMessage={users.messages[users.messages.length-1]} />
+			))}
 			</div>
 		)
 	}
 }
+
+
 
 
 export default UserList;
