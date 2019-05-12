54a447a206eefe934db8f06f24e9dab954b52fdd
Diuda <divyanshu.1996@gmail.com>
2019-05-12 20:53:03 +0530

    message recieved reducer initialized

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,4 +1,4 @@
-import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE, USERS_FETCHED, LOAD_CHAT_HISTORY, MESSAGE_SENT } from '../constants/constant';
+import { SEND_MESSAGE, RECIEVE_MESSAGE, LOAD_MESSAGE, USERS_FETCHED, LOAD_CHAT_HISTORY, MESSAGE_SENT, MESSAGE_RECIEVED } from '../constants/constant';
 
 
 const initialState = {
@@ -10,7 +10,7 @@ const initialState = {
 //TODO hafl the things
 export default function chat(state=initialState, { type, payload }) {
 	switch (type) {
-		case MESSAGE_SENT: {
+		case MESSAGE_SENT:{
 				let newValue = state.users.map(user => {
 					if(user.id === state.activeUser){
 						return {
@@ -25,7 +25,19 @@ export default function chat(state=initialState, { type, payload }) {
 
 
 		}
-		case RECIEVE_MESSAGE: return { users: [...state.users, payload] };
+		case MESSAGE_RECIEVED:{
+			let newValue = state.users.map(user => {
+				if(user.id === state.activeUser){
+					return {
+						...user,
+						messages: [...user.messages, payload]
+					}
+				}
+				return user;
+			})
+
+			return { ...state, users: newValue };
+		}
 		case LOAD_MESSAGE: return { users: payload };
 		case USERS_FETCHED: return { ...state, users: payload } ;
 		case LOAD_CHAT_HISTORY: return {...state, activeUser: payload };
