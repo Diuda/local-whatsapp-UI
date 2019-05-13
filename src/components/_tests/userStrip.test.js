03ada6a8570c72b14bab477b19df58fdc7c44611
Diuda <divyanshu.1996@gmail.com>
2019-05-13 15:59:03 +0530

    WIP test user strip component

---
+++ src/components/_tests/userStrip.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { configure, shallow, render} from 'enzyme';
+import chai, { expect } from 'chai';
+import { Provider } from "react-redux";
+import store from "../../store/index";
+import UserStrip from '../userStrip';
+
+// var App = require('./containers/App');
+import chaiEnzyme from 'chai-enzyme';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('User Strip Component testing', function() {
+
+	it('should return image, name and latest message', () => {
+		const wrapper = shallow(<UserStrip store={store}/>);
+		const editText = <div className='parent-wrapper'></div>;
+		expect(wrapper).to.contain(editText);
+
+});
+
+chai.use(chaiEnzyme())
+
+})
