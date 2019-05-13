d8fb7216dca2469da9aa0deb10c6465572b013b8
Diuda <divyanshu.1996@gmail.com>
2019-05-13 15:57:42 +0530

    add transform class properties

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,16 @@
 module.exports = function (api) {
-	const presets = ["react-app"];
+	const presets = ["react-app", "@babel/preset-env", "@babel/preset-react"];
 	const plugins = [
 		"@babel/plugin-transform-modules-commonjs", 
-		"inline-react-svg"
+		"inline-react-svg",
+		"transform-class-properties"
 	];
   
 	/** this is just for minimal working purposes, 
 	 * for testing larger applications it is
 	 * advisable to cache the transpiled modules in
 	 * node_modules/.bin/.cache/@babel/register**/
-	// api.cache(false); 
+	api.cache(false); 
   
 	return {
 		presets, 
