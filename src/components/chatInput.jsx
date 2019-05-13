e0d1d69cd4c00f6ace8c3460718d5b8c15a3a296
Diuda <divyanshu.1996@gmail.com>
2019-05-13 19:23:03 +0530

    add conditional send button

--- src/components/chatInput.jsx
+++ src/components/chatInput.jsx
@@ -3,25 +3,63 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { sendMessage } from '../actions/index';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLaugh, faMicrophone, faPaperPlane  } from '@fortawesome/free-solid-svg-icons';
+
 
 class ChatInput extends React.Component {
 
+	constructor(){
+		super();
+		this.state = {
+			isTyping: false
+		}
+	}
+
 
 	handleSendMessage = (e) => {
+		if(e.target.value){
+			this.setState({isTyping: true})
+		}
+		else{
+			this.setState({isTyping: false})
+		}
 		if(e.which === 13){
-			this.props.sendMessage(this.props.activeUser, e.target.value)
+			this.props.sendMessage(this.props.activeUser, e.target.value);
 			e.target.value = '';
 		}
+	}
+
+	handleSendMessageFromButton = (e) => {
+		this.props.sendMessage(this.props.activeUser, this.messageInput.value);
+		this.messageInput.value = '';
 	}
 
 	render() {
 		return(
 			<div className='chat-wrapper'>
-				<div className='emoji-btn'></div>
-				<div className='chat-input-wrapper'>
-					<input type='text' autoComplete='off' name='chat-input' placeholder='Type a message' onKeyPress={this.handleSendMessage}/>
+				<div className='emoji-btn'>
+					<FontAwesomeIcon icon={faLaugh}/>
 				</div>
-				<div className='voice-btn'></div>
+				<div className='chat-input-wrapper'>
+					<input type='text' autoComplete='off' name='chat-input' placeholder='Type a message' ref={(el) => {this.messageInput = el;}} onKeyPress={this.handleSendMessage}/>
+				</div>
+				{
+					this.state.isTyping?
+					(
+						<div className='send-btn'>
+							<button onClick = {this.handleSendMessageFromButton}>
+								<FontAwesomeIcon icon={faPaperPlane} />
+							</button>
+						</div>
+					):
+					(
+						
+						<div className='voice-btn'>
+								<FontAwesomeIcon icon={faMicrophone} />
+						</div>
+					)
+				}
 			</div>
 		)
 	}
