116dd56c58c48f7980be8a502a556eafbd175bb8
Diuda <divyanshu.1996@gmail.com>
2019-05-13 22:44:14 +0530

    uuid included in message id

--- src/model/db.js
+++ src/model/db.js
@@ -1,8 +1,10 @@
 import Dexie from 'dexie';
+import uuid from 'uuid';
+
 
 const db = new Dexie('UserData');
 
-db.version(1).stores({user: "++id, name, *messages, messages.text, messages.time, messages.action"});
+db.version(1).stores({user: "++id, name, *messages, messages.text, messages.action, messages.id"});
 
 
 
@@ -18,11 +20,11 @@ const addUser = (user) => {
 
 
 //hardcoding users and initial messages
-// addUser({name: 'div', imageURL: 'profile1.jpg', messages:[{'text': 'hello', 'action': 'sent'}]});
-// addUser({name: 'paddy', imageURL: 'profile2.jpg', messages:[{'text': 'hi', 'action': 'sent'}]});
-// addUser({name: 'achlendra', imageURL: 'profile3.jpg', messages:[{'text': 'bye', 'action': 'sent'}]});
-// addUser({name: 'sinha', imageURL: 'profile4.jpg', messages:[{'text': 'sup', 'action': 'sent'}]});
-// addUser({name: 'prakhar', imageURL: 'profile5.jpg', messages:[{'text': 'android', 'action': 'sent'}]});
-// addUser({name: 'saujanya', imageURL: 'profile.jpg', messages:[{'text': 'cool', 'action': 'sent'}]});
-// addUser({name: 'wally', imageURL: 'profile7.jpg', messages:[{'text': 'sure man', 'action': 'sent'}]});
+// addUser({name: 'div', imageURL: 'profile1.jpg', messages:[{ 'id': uuid.v4(), 'text': 'hello', 'action': 'sent'}]});
+// addUser({name: 'paddy', imageURL: 'profile2.jpg', messages:[{ 'id': uuid.v4(), 'text': 'hi', 'action': 'sent'}]});
+// addUser({name: 'achlendra', imageURL: 'profile3.jpg', messages:[{ 'id': uuid.v4(), 'text': 'bye', 'action': 'sent'}]});
+// addUser({name: 'sinha', imageURL: 'profile4.jpg', messages:[{ 'id': uuid.v4(), 'text': 'sup', 'action': 'sent'}]});
+// addUser({name: 'prakhar', imageURL: 'profile5.jpg', messages:[{ 'id': uuid.v4(), 'text': 'android', 'action': 'sent'}]});
+// addUser({name: 'saujanya', imageURL: 'profile.jpg', messages:[{ 'id': uuid.v4(), 'text': 'cool', 'action': 'sent'}]});
+// addUser({name: 'wally', imageURL: 'profile7.jpg', messages:[{ 'id': uuid.v4(), 'text': 'sure man', 'action': 'sent'}]});
 export default db;
